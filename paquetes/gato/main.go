7dc69c97ec7ddffdd465135017c6b87092016b0a
mateobr0 <chuttmateo0@gmail.com>
2023-05-02 21:48:41 -0300

    un simple paquete

---
+++ paquetes/gato/main.go
@@ -0,0 +1,5 @@
+package gato
+import "fmt"
+func Hola(){
+	fmt.Println(`soy un gato y estoy en el paquete "gato"`)
+}
---
+++ paquetes/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mateobr0/curso.go/paquetes/gato"
+
+func main() {
+	gato.Hola()
+}
