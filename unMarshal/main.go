235368ce4947fc4266dcee4b21c4eb0d1e9d8862
mateobr0 <chuttmateo0@gmail.com>
2023-05-02 22:07:23 -0300

    Argentina campeón

--- unMarshal/main.go
+++ unMarshal/main.go
@@ -4,32 +4,56 @@ import (
 	"encoding/json"
 	"fmt"
 )
-type persona struct{
-	Nombre string `json:"Nombre"`
-	Apellido string `json:"Apellido"`
-	Edad int `json:"Edad"`
-	Dichos []string `json:"Dichos"`
+
+type pais struct {
+	Nombre           string  `json:"nombre"`
+	Tamano           string  `json:"tamano"`
+	Poblacion        float64 `json:"poblacion"`
+	MundialesGanados int     `json:"mundiales_ganados"`
 }
 
 func main() {
-	s := `[{"Nombre":"Eduar","Apellido":"Tua","Edad":32,"Dichos":["Cachicamo diciéndole a morrocoy conchudo","La mona, aunque se vista de seda, mona se queda","Poco a poco se anda lejos"]},
-	{"Nombre":"Carlos","Apellido":"Pérez","Edad":27,"Dichos":["Camarón que se duerme se lo lleva la corriente","A ponerse las alpargatas que lo que viene es joropo","No gastes pólvora en zamuro"]},
-	{"Nombre":"M","Apellido":"Hmmmm","Edad":54,"Dichos":["Ni lava ni presta la batea","Hijo de gato, caza ratón","Más vale pájaro en mano que cien volando"]}]`
-	//fmt.Println(s)
-	
-	var personas []persona
-	err := json.Unmarshal([]byte(s), &personas)
+	paisesjson := `[
+    {
+      "nombre": "Brasil",
+      "tamano": "8.515.767 km²",
+      "poblacion": 213.3,
+      "mundiales_ganados": 5
+    },
+    {
+      "nombre": "Argentina",
+      "tamano": "2.780.400 km²",
+      "poblacion": 45.5,
+      "mundiales_ganados": 3
+    },
+    {
+      "nombre": "Alemania",
+      "tamano": "357.386 km²",
+      "poblacion": 83.2,
+      "mundiales_ganados": 4
+    },
+    {
+      "nombre": "Italia",
+      "tamano": "301.340 km²",
+      "poblacion": 60.4,
+      "mundiales_ganados": 4
+    },
+    {
+      "nombre": "Francia",
+      "tamano": "643.801 km²",
+      "poblacion": 66.99,
+      "mundiales_ganados": 2
+    }
+  ]`
+	var paises []pais
+
+	err := json.Unmarshal([]byte(paisesjson), &paises)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("error:", err)
 	}
-	fmt.Println(personas)
-	for _, v := range personas{
-		fmt.Println(v.Nombre)
-		fmt.Println(v.Apellido)
-		fmt.Println(v.Edad)
-		for _, dicho := range v.Dichos{
-			fmt.Printf("\t%v\n", dicho)
-		}
+	fmt.Printf("%+v\n", paises)
+	for _, v := range paises {
+		fmt.Println(v)
 	}
+
 }
