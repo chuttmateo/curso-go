4ff0d9a369130e8e52309e810fc74bc30c8ee9e0
mateobr0 <chuttmateo0@gmail.com>
2023-05-11 18:51:56 -0300

    gitignore

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
 )
 
+var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
+
+var templ = template.Must(template.New("qr").Parse(templateStr))
+
 func main() {
-	c := []int{1, 2, 3, 4, 6, 7}
-	foo(c...)
-}
-func foo(x ...int)  {
-	for v, i := range x{
-		fmt.Println(v, i)
+	flag.Parse()
+	http.Handle("/", http.HandlerFunc(QR))
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
 }
+
+func QR(w http.ResponseWriter, req *http.Request) {
+	templ.Execute(w, req.FormValue("s"))
+}
+
+const templateStr = `
+<html>
+<head>
+<title>QR Link Generator</title>
+</head>
+<body>
+{{if .}}
+<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
+<br>
+{{.}}
+<br>
+<br>
+{{end}}
+<form action="/" name=f method="GET">
+    <input maxLength=1024 size=70 name=s value="" title="Text to QR Encode">
+    <input type=submit value="Show QR" name=qr>
+</form>
+</body>
+</html>
+`
