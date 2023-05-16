21413b4dfd62d1a5df67351c300efc52c07eb3be
mateobr0 <chuttmateo0@gmail.com>
2023-05-16 20:41:19 -0300

    fibonacci y números primos

---
+++ ejercicios/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mateobr0/curso.go/ejercicios/fibonacci"
+)
+
+func main() {
+	/* fmt.Println("no es primo", primo.EsPrimo(1))
+	fmt.Println("es primo", primo.EsPrimo(2))
+	fmt.Println("es primo", primo.EsPrimo(3))
+	fmt.Println("no es primo", primo.EsPrimo(4))
+	fmt.Println("es primo", primo.EsPrimo(5))
+	fmt.Println("no es primo", primo.EsPrimo(6))
+	fmt.Println("es primo", primo.EsPrimo(7))
+	fmt.Println("no es primo", primo.EsPrimo(8))
+	fmt.Println("no es primo", primo.EsPrimo(9))
+	fmt.Println("no es primo", primo.EsPrimo(10))
+	fmt.Println("es primo", primo.EsPrimo(11))
+	fmt.Println("no es primo", primo.EsPrimo(12))
+	fmt.Println("es primo", primo.EsPrimo(13))
+	fmt.Println("no es primo", primo.EsPrimo(14))
+	fmt.Println("no es primo", primo.EsPrimo(15))
+	fmt.Println("no es primo", primo.EsPrimo(16))
+	fmt.Println("es primo", primo.EsPrimo(17))
+	fmt.Println("no es primo", primo.EsPrimo(18))
+	fmt.Println("es primo", primo.EsPrimo(19))
+	fmt.Println("no es primo", primo.EsPrimo(20)) */
+	fmt.Println(fibonacci.SecuenciaFibonacci(10))
+}
