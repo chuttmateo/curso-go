0eb7d9cda19621a163297079dd2b1afd9b0fa167
mateobr0 <chuttmateo0@gmail.com>
2023-06-06 21:37:37 -0300

    ejercicio de callback

--- callbacks/main.go
+++ callbacks/main.go
@@ -1,10 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main(){
-	fmt.Println("sadfasdf")
-	
+func main() {
+	fmt.Println(sumaDePares(suma, []int{1, 2, 3, 4, 5, 6}...))
+}
+
+func suma(nums ...int) int {
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	return total
+}
+
+func sumaDePares(f func(...int) int, nums ...int) int {
+	var y []int
+	for _, v := range nums {
+		if v%2 == 0 {
+			y = append(y, v)
+		}
+	}
+	return f(y...)
 }
